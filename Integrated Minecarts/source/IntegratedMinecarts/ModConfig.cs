aa77eab680b7911b84e2f99442efb091019a3d70
Jibb <103417406+Jibblestein@users.noreply.github.com>
2024-04-19 20:14:16 +0800

    uploading source for C# component

---
+++ Integrated Minecarts/source/IntegratedMinecarts/ModConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegratedMinecarts
+{
+    public sealed class ModConfig
+    {
+        public int DestinationsPerPage { get; set; } = 5;
+    }
+}
